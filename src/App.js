8c238b4aa2e68e77cd23d6a58444afd34d909167
Sanu <sanusatyadarshi@gmail.com>
2019-03-26 15:54:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            are you kidding me???
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sleep React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
