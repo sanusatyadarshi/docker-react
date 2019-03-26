e2e68302c1a7dad6213e872ae06b847a898ab4d3
Sanu <sanusatyadarshi@gmail.com>
2019-03-26 15:57:29 +0530

    first commit

---
+++ build/precache-manifest.d9ae4193e9592e7dde363de7688d13f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1c046cae12f5f31c5782",
+    "url": "/static/js/main.3e7b2740.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "1c046cae12f5f31c5782",
+    "url": "/static/css/main.172da42a.chunk.css"
+  },
+  {
+    "revision": "b0a3bddc016bcca40126d9b0662bfdd2",
+    "url": "/index.html"
+  }
+];
